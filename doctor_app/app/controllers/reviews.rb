f293e94c129d56128c6d7305fcd9b1da276175c5
Domonique Griffin <griffind2009@gmail.com>
2016-10-31 11:06:59 -0400

    finished controllers

--- doctor_app/app/controllers/reviews.rb
+++ doctor_app/app/controllers/reviews.rb
@@ -1,2 +1,47 @@
 class ReviewsController < ApplicationController
+  def index
+    @doctor = Doctor.find(params[:doctor_id])
+    @reviews = @doctor.reviews
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @reviews }
+    end
+    end
+  def show
+    @doctor = Doctor.find(params[:doctor_id])
+    @review = Review.find(params[:id])
+ respond_to do |format|
+   format.html { render :show }
+   format.json { render json: @review }
+ end
+  end
+  def new
+    @doctor = Doctor.find(params[:doctor_id])
+    @review = Review.new
+  end
+def create
+    @doctor = Doctor.find(params[:doctor_id])
+    @review = @doctor.reviews.create!(review_params)
+    redirect_to review_path(@review)
+end
+def edit
+  @review = Review.find(params[:id])
+
+end
+def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to review_path(@review)
+end
+def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+end
+
+
+private
+def review_params
+  params.require(:review).permit(:name, :body)
+end
 end
--- doctor_app/config/routes.rb
+++ doctor_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :doctors do
+  resources :reviews
+end
+root to: redirect('/doctors')
 end
