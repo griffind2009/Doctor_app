162ccf9217199f820bc5a5c2ee7ebe78cb862f39
Domonique Griffin <griffind2009@gmail.com>
2016-10-31 10:13:48 -0400

    models and controllers defined

---
+++ doctor_app/app/models/doctor.rb
@@ -0,0 +1,5 @@
+class Doctor < ActiveRecord::Base
+  has_many :reviews
+  url = "https://api.betterdoctor.com/2016-03-01/doctors?location=38.907192%2C-77.036871%2C100&user_location=38.907192%2C-77.036871&skip=0&limit=10&user_key=#{ENV["doctor_api_key"]}
+   response = HTTParty.get(url)
+end
